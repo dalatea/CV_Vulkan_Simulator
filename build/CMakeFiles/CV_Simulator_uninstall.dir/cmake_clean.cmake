230b50cb162f3d239ce08e8c9624eb42e4cb05ef
Dalatea <148526614+dalatea@users.noreply.github.com>
2026-04-05 18:04:28 +0300

    Delete build directory

--- build/CMakeFiles/CV_Simulator_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/CV_Simulator_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/CV_Simulator_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
